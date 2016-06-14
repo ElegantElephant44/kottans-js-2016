588885798aa6f649497c587f8de32df0f3bb3fd0
Sergey <sergeypuhl1995@gmail.com>
2016-06-14 17:45:03 +0300

    Remade deepAssign with "new function"

--- week03/homework1/deepAssign.js
+++ week03/homework1/deepAssign.js
@@ -6,36 +6,33 @@ function deepAssign(target, ...sources) {
     }
     var to = Object(target)
     sources.forEach(function(from) {
-        deepCopy(to, from)
+        new function deepCopy(to, from) {
+            if (from !== Object(from)) return
+            Reflect.ownKeys(from).forEach(function(key) {
+
+                if (!isEnumerable.call(from, key)) return
+
+                let value = from[key];
+                if (value !== Object(value)) {
+                    to[key] = value;
+                    return
+                }
+
+                if (value instanceof RegExp || value instanceof Date ||
+                    value instanceof Map || value instanceof Set) {
+                    to[key] = new value.constructor(value)
+                    return
+                }
+
+                if (to[key] === undefined) to[key] = new value.constructor()
+
+                if (to[key] !== Object(to[key]))
+                    to[key] = value
+                else
+                    deepCopy(to[key], value)
+            })
+        }(to, from)
     })
     return to
 }
-
-function deepCopy(to, from) {
-    if (from !== Object(from)) return
-    Reflect.ownKeys(from).forEach(function(key) {
-
-        if (!isEnumerable.call(from, key)) return
-
-        let value = from[key];
-        if (value !== Object(value)) {
-            to[key] = value;
-            return
-        }
-
-        if (value instanceof RegExp || value instanceof Date ||
-            value instanceof Map || value instanceof Set) {
-            to[key] = new value.constructor(value)
-            return
-        }
-
-        if (to[key] === undefined) to[key] = new value.constructor()
-
-        if (to[key] !== Object(to[key]))
-            to[key] = value
-        else
-            deepCopy(to[key], value)
-    })
-}
-
 console.log(deepAssign({a: {b: 0, c:1}}, {a: {b: {d: 1}, c: 2}}, {a:{c: 3}}))
